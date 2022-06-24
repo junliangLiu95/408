0205d0ac3f33d3fd1fc04f9ec7efc2b795024296
junliang.liu <junliang.liu@lotusflare.com>
2022-06-24 14:22:08 +0800

    finish linear_list

--- linear_list.c
+++ linear_list.c
@@ -3,6 +3,14 @@
 #include <stdbool.h>
 #define LIST_INIT_SIZE 100
 #define LISTINCREMENT 10
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASIBLE -1
+#define OVERFLOW -2
+
+typedef int Status;
 
 typedef struct
 {
@@ -11,22 +19,22 @@ typedef struct
     int listsize;
 } SqList;
 
-int InitList_Sq(SqList *L)
+Status InitList_Sq(SqList *L)
 {
     L->elem = (int *)malloc(LIST_INIT_SIZE * sizeof(int));
     if (!L->elem)
         exit(0);
     L->length = 0;
     L->listsize = LIST_INIT_SIZE;
-    return 1;
+    return OK;
 }
 
-int DestroyList(SqList *L)
+Status DestroyList(SqList *L)
 {
     free(L->elem);
     L->length = 0;
     L->listsize = LIST_INIT_SIZE;
-    return 0;
+    return OK;
 }
 
 void ClearList(SqList *L)
@@ -132,15 +140,68 @@ int *ListDelete_Sq(SqList *L, int i, int *e)
     return e;
 }
 
+/*
+      *a
+    La 2 3 8 9
+      *b
+    Lb 1 2 4 5 6 7
+      *c
+    Lc 1
+*/
+
+void MergeList_Sq(SqList La, SqList Lb, SqList *Lc)
+{
+    int *a = La.elem;
+    int *b = Lb.elem;
+    Lc->listsize = Lc->length = La.length + Lb.length;
+    Lc->elem = (int *)malloc(Lc->listsize * sizeof(int));
+    int *c = Lc->elem;
+    if (!Lc->elem)
+        exit(OVERFLOW);
+    int *La_last = a + La.length - 1;
+    int *Lb_last = b + Lb.length - 1;
+    while (a <= La_last && b <= Lb_last)
+    {
+        if (*a <= *b)
+        {
+            *c++ = *a++;
+        }
+        else
+        {
+            *c++ = *b++;
+        }
+    }
+    while (a <= La_last)
+    {
+        *c++ = *a++;
+    }
+    while (b <= Lb_last)
+    {
+        *c++ = *b++;
+    }
+}
+
 int main()
 {
-    SqList p;
-    InitList_Sq(&p);
+    SqList a, b, c;
+    InitList_Sq(&a);
+    InitList_Sq(&b);
+    InitList_Sq(&c);
     for (int i = 1; i <= 5; i++)
     {
-        printf("%d", i);
-        ListInsert_Sq(&p, i, i);
+        printf("%d ", i);
+        ListInsert_Sq(&a, i, i);
     }
-    int index = LocateElem(p, 4, compare);
-    printf("%d", index);
+    printf("\n");
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("%d ", i * 2);
+        ListInsert_Sq(&b, i, i * 2);
+    }
+    printf("\n");
+    MergeList_Sq(a, b, &c);
+    for (int i = 0; i < c.length; i++)
+    {
+        printf("%d ", c.elem[i]);
+    }
 }
