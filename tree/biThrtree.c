7252b1d466b595b6e1373b606779d54f558e177e
junliang.liu <junliang.liu@lotusflare.com>
2022-12-01 15:17:38 +0800

    finish thread tree

--- tree/biThrtree.c
+++ tree/biThrtree.c
@@ -5,11 +5,26 @@ void visit(TElemType str)
     printf("%c,", str);
 }
 
+/*
+             1
+        2         3
+      4    5   6     7
+        9
+*/
+
 int main()
 {
     int index = 0;
-    BiThrTree T;
+    BiThrTree T, Thrt, current, pre, next;
     CreateBiThrTree(&T, &index);
-    BiThrTree_PreOrder(T, visit);
+    BiThrTree_InOrder(T, visit);
+    printf("\n");
+    InOrderThreading(&Thrt, T);
+    InOrderThrTreeTraverse(Thrt);
+    printf("\n");
+    current = T->lchild;
+    pre = BiThrTree_InOrderPre(current);
+    next = BiThrTree_InOrderNext(current);
+    printf("current: %c, pre: %c, next: %c\n", current->data, pre->data, next->data);
     return 0;
 }
--- tree/biThrtree.h
+++ tree/biThrtree.h
@@ -50,12 +50,109 @@ void CreateBiThrTree(BiThrTree *T, int *index)
     }
 }
 
-void BiThrTree_PreOrder(BiThrNode *T, void (*visit)(TElemType))
+void BiThrTree_InOrder(BiThrNode *T, void (*visit)(TElemType))
 {
     if (T)
     {
+        BiThrTree_InOrder(T->lchild, visit);
         visit(T->data);
-        BiThrTree_PreOrder(T->lchild, visit);
-        BiThrTree_PreOrder(T->rchild, visit);
+        BiThrTree_InOrder(T->rchild, visit);
     }
+}
+
+/*
+      lchild--1 Thrt rchild--7
+      4 lchild ---Thrt
+      7 rchild ---Thrt
+
+             1
+        2         3
+      4    5   6     7
+        9
+*/
+
+void InThreading(BiThrTree p, BiThrTree *pre)
+{
+    if (p)
+    {
+        InThreading(p->lchild, pre);
+        if (!p->lchild)
+        {
+            p->LTag = Thread;
+            p->lchild = (*pre);
+        }
+        if (!(*pre)->rchild)
+        {
+            (*pre)->RTag = Thread;
+            (*pre)->rchild = p;
+        }
+        (*pre) = p;
+        InThreading(p->rchild, pre);
+    }
+}
+
+Status InOrderThreading(BiThrTree *Thrt, BiThrTree T)
+{
+    *Thrt = (BiThrTree)malloc(sizeof(BiThrNode));
+    (*Thrt)->LTag = Link;
+    (*Thrt)->RTag = Thread;
+    (*Thrt)->lchild = (*Thrt);
+    if (!T)
+    {
+        (*Thrt)->lchild = (*Thrt);
+    }
+    else
+    {
+        (*Thrt)->lchild = T;
+        BiThrTree pre = (*Thrt);
+        InThreading(T, &pre);
+        (*Thrt)->rchild = pre;
+        pre->RTag = Thread;
+        pre->rchild = (*Thrt);
+    }
+    return OK;
+}
+
+Status InOrderThrTreeTraverse(BiThrTree T)
+{
+    BiThrTree p = T->lchild;
+    while (p != T)
+    {
+        while (p->LTag == Link)
+        {
+            p = p->lchild;
+        }
+        printf("%c,", p->data);
+        while (p->RTag == Thread && p->rchild != T)
+        {
+            printf("%c,", p->rchild->data);
+            p = p->rchild;
+        }
+        p = p->rchild;
+    }
+    return OK;
+}
+
+BiThrTree BiThrTree_InOrderPre(BiThrTree T)
+{
+    if (T->LTag == Thread)
+        return T->lchild;
+    BiThrTree p = T->lchild;
+    while (p->RTag == Link)
+    {
+        p = p->rchild;
+    }
+    return p;
+}
+
+BiThrTree BiThrTree_InOrderNext(BiThrTree T)
+{
+    if (T->RTag == Thread)
+        return T->rchild;
+    BiThrTree p = T->rchild;
+    while (p->LTag == Link)
+    {
+        p = p->lchild;
+    }
+    return p;
 }
